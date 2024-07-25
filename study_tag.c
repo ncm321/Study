e86b1c85a6f3dc829e758b12ec8d089b2c0304f6
niuchunmin <2430815397@qq.com>
2024-07-25 15:57:02 +0800

    11

---
+++ study_tag.c
@@ -0,0 +1,21 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2024 niuchunmin
+ *                  All rights reserved.
+ *
+ *       Filename:  study_tag.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(25/07/24)
+ *         Author:  Niu Chunmin <niuchunmin@2430815397@qq.com>
+ *      ChangeLog:  1, Release initial version on "25/07/24 15:50:00"
+ *                 
+ ********************************************************************************/
+
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+	printf ("study tag!!\n");
+	return 0;
+} 
+
